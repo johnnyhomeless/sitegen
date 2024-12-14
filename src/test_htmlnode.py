36d2dacb90d8e36f0b9a8d379393b1936631a069
johnnynocash <giovolo@gmail.com>
2024-12-14 07:00:23 +0100

    Integrato LeafNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_empty_props(self):
@@ -12,6 +12,30 @@ class TestHTMLNode(unittest.TestCase):
 
     def test_multiple_props(self):
         node = HTMLNode(props={"id": "test", "id1": "test1"})
+
+class TestLeafNode(unittest.TestCase):
+    def test_tag_value(self):
+        node = LeafNode("a", "Search here!", {"href": "https://www.google.com"})
+        result = '<a href="https://www.google.com">Search here!</a>'
+        self.assertEqual(node.to_html(), result)
+
+    def test_tag_noprops(self):
+        node = LeafNode("p", "This is gasoline, and I put myself on fire")
+        result = '<p>This is gasoline, and I put myself on fire</p>'
+        self.assertEqual(node.to_html(), result)
+    
+    def test_notag(self):
+        node = LeafNode(None, "This is gasoline, and I put myself on fire")
+        result = "This is gasoline, and I put myself on fire"
+        self.assertEqual(node.to_html(), result)
+    
+    def test_missing_value(self):
+        with self.assertRaises(ValueError):
+            node = LeafNode("p", None)
+            node.to_html()
+    
+    
+
 
 if __name__ == "__main__":
     unittest.main()
